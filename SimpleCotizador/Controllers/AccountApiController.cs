bc7660ee67753b6684cb81de823cb4394a90d984
Andreeto, Enzo <Enzo.Andreeto@VistraEnergy.com>
2018-06-29 11:49:53 -0300

    Crear api de login

---
+++ SimpleCotizador/Controllers/AccountApiController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleCotizador.DTOs;
+using SimpleCotizador.Models;
+
+namespace SimpleCotizador.Controllers
+{
+    //[Produces("application/json")]
+    //[Route("api/AccountApi")]
+    public class AccountApiController : ControllerBase
+    {
+        private SignInManager<SimpleCotizadorUser> _signInManager;
+
+        private UserManager<SimpleCotizadorUser> _userManager;
+
+        public AccountApiController(SignInManager<SimpleCotizadorUser> signInManager, UserManager<SimpleCotizadorUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("SimpleCotizadorApi/Account/Login")]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginRequest loginRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                await _signInManager.SignOutAsync();
+
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginRequest.Usuario,
+                                                                                                             loginRequest.Contrasenia,
+                                                                                                             isPersistent: false,
+                                                                                                             lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    SimpleCotizadorUser usuario = await _userManager.FindByNameAsync(loginRequest.Usuario);
+                    return Ok(new Dictionary<string, string> { { "Usuario", usuario.UserName } });
+                }
+                else
+                {
+                    return BadRequest("Usuario o contraseña inválidos.");
+                }
+            }
+        }
+    }
+}
---
+++ SimpleCotizador/DTOs/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCotizador.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Usuario { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Contrasenia { get; set; }
+    }
+}
