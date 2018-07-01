a61fdeb01469647803d6961495033d0e52af4559
Andreeto, Enzo <Enzo.Andreeto@VistraEnergy.com>
2018-07-01 16:15:39 -0300

    Mostrar usuario loggeado en el menu

--- SimpleCotizador/Controllers/HomeController.cs
+++ SimpleCotizador/Controllers/HomeController.cs
@@ -8,25 +8,12 @@ namespace SimpleCotizador.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string usuario)
         {
+            ViewBag.Usuario = usuario;
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
